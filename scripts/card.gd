3545a9bbc11d358c5abe41e5ef68723616eb8fc4
Josie Barber <barbe497@umn.edu>
2025-01-30 14:47:27 -0600

    Add card.gd
    Add the card class file and the required fields

---
+++ scripts/card.gd
@@ -0,0 +1,12 @@
+#Card.gd
+
+extends Node2D
+
+class_name card
+
+var card_name: String
+var card_effect: String
+var card_clause: String
+var card_type: String
+var card_sprite: String
+var card_damage: int
