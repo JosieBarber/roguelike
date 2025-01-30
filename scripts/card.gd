a3ac47a2934f23b81a9a398e6895b406896b4012
Josie Barber <barbe497@umn.edu>
2025-01-30 14:53:57 -0600

    Add _init() function to card.gd
    Make sure you can initialize a card with intended values

--- scripts/card.gd
+++ scripts/card.gd
@@ -10,3 +10,15 @@ var card_clause: String
 var card_type: String
 var card_sprite: String
 var card_damage: int
+
+func _init(card_name_param: String, effect: String, clause: String, type: String, sprite: String, damage: int):
+    card_name = card_name_param
+    card_effect = effect
+    card_clause = clause
+    card_type = type
+    card_sprite = sprite
+    card_damage = damage
+
+func _ready():
+    # Initialize the card when it is added to the scene
+    pass
