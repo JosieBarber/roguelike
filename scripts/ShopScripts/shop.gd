9b5077f7073bf6d7810d3481da788494f7ad6875
Josie Barber <barbe497@umn.edu>
2025-02-21 16:20:24 -0600

    Revert "Change shop to have same testUI"
    This reverts commit cbe947301d505a89f497ebe0e7d641a5616d9969.

--- scripts/ShopScripts/shop.gd
+++ scripts/ShopScripts/shop.gd
@@ -4,7 +4,6 @@ extends Node2D
 var player: Player
 var test_cards = []
 var displayed_cards = []
-var card_rects: Array = []
 
 func _ready():
 	if get_parent().get_node("Player"):
@@ -45,32 +44,21 @@ func display_random_cards():
 		test_cards.remove_at(random_index)
 	print("Random cards: ", random_cards)
 	
-	var item_list = get_node("ItemList")
-	card_rects.clear()
-	for child in item_list.get_children():
-		child.queue_free()
-	for i in range(random_cards.size()):
-		var card_instance = random_cards[i].new()
-		var card_display = CardDisplay.new(card_instance.card_name, card_instance.damage)
-		card_display.position = Vector2(10, (i * 60) + 20)
-		var hitbox_position = Vector2(0, 0)
-		var hitbox = Area2D.new()
-		var collision_shape = CollisionShape2D.new()
-		collision_shape.shape = RectangleShape2D.new()
-		collision_shape.shape.extents = Vector2(100, 10)
-		hitbox.position = hitbox_position
-		hitbox.add_child(collision_shape)
-		card_display.add_child(hitbox)
-		
+	for card_script in random_cards:
+		var card_instance = card_script.new()
+		var card_display = Button.new()
+		card_display.text = card_instance.card_name
+		add_child(card_display)
+		displayed_cards.append(card_display)
+
+		card_display.position = Vector2(150, 100 * displayed_cards.size())
 		var value_label = Label.new()
 		value_label.text = str(card_instance.value)
 		value_label.name = "ValueLabel"
 		card_display.add_child(value_label)
 		value_label.position = Vector2(10, 40)
-		
-		card_rects.append(hitbox)
-		item_list.add_child(card_display)
-		displayed_cards.append(card_display)
+
+		card_display.connect("pressed", Callable(self, "_on_Card_pressed").bind(card_instance, card_display))
 
 	update_card_display_colors()
 
@@ -83,18 +71,7 @@ func update_card_display_colors():
 		else:
 			value_label.modulate = Color(1, 1, 1)
 
-func _input(event):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		for i in range(card_rects.size()):
-			var global_position = card_rects[i].get_global_transform_with_canvas().origin
-			var global_rect = Rect2(global_position, Vector2(200, 30))
-			if global_rect.has_point(event.position):
-				_on_Card_pressed(i)
-				break
-
-func _on_Card_pressed(card_index: int):
-	var card_display = displayed_cards[card_index]
-	var card_instance = card_display.card_instance
+func _on_Card_pressed(card_instance, card_display):
 	var card_value = card_instance.value
 	if player.health > card_value:
 		player.health -= card_value
