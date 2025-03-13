dab0cb7668481290b458b8fb47c25a2040619e59
Josie Barber <barbe497@umn.edu>
2025-03-13 15:46:34 -0500

    Update main.gd to fit @onready style

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,15 +1,9 @@
 extends Node2D
 
-var player_instance = Player
+@onready var player_instance: Player = get_node("Player")
+@onready var navigation_scene = get_node("Navigation")
 
 func _ready():
-	player_instance = get_node("Player")
 	player_instance.initialize("JosiePosie", 20)
 	player_instance._create_test_deck()
-	# player_instance.copy_deck()
-	
-	# Start with the navigation scene
-	var navigation_scene = get_node("Navigation")
 	navigation_scene.visible = true
-
-	# player_instance.set_health(20)
