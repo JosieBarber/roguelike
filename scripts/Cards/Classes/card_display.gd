026cdf15c139213170db07b56079a45e18a1782d
Josie Barber <barbe497@umn.edu>
2025-03-13 17:30:43 -0500

    Fix issue with overlapping cards when hovering

--- scripts/Cards/Classes/card_display.gd
+++ scripts/Cards/Classes/card_display.gd
@@ -6,6 +6,8 @@ signal card_clicked(card, parent_node)
 
 var card: Card
 var hoverable: bool
+static var current_hovered_card: CardDisplay = null
+var is_mouse_pressed: bool = false
 
 func _ready():
 	var area = $Area2D
@@ -22,16 +24,32 @@ func _ready():
 	var damage_label = $DamageLabel
 	damage_label.text = str(card.damage)
 
+func _process(delta):
+	if current_hovered_card != null and not current_hovered_card.hoverable:
+		# Reset hover state if the current hovered card is no longer hoverable
+		current_hovered_card._reset_hover_state()
+
 func _on_area_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		emit_signal("card_clicked", card, get_parent())
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			is_mouse_pressed = true
+		elif not event.pressed and is_mouse_pressed:
+			is_mouse_pressed = false
+			emit_signal("card_clicked", card, get_parent())
 
 func _on_mouse_entered():
 	if hoverable:
+		if current_hovered_card != null and current_hovered_card != self:
+			current_hovered_card._reset_hover_state()
+		current_hovered_card = self
 		scale = Vector2(scale[0] * 1.2, scale[1] * 1.2)
 		z_index = 100
 
 func _on_mouse_exited():
-	if hoverable:
-		scale = Vector2(scale[0] * (1/1.2), scale[1] * (1/1.2))
-		z_index = 0
+	if hoverable and current_hovered_card == self:
+		_reset_hover_state()
+
+func _reset_hover_state():
+	current_hovered_card = null
+	scale = Vector2(scale[0] * (1/1.2), scale[1] * (1/1.2))
+	z_index = 0
