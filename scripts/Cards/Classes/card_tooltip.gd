34b37042cae79ffee4d8192da3233a0e4c148534
Josie Barber <barbe497@umn.edu>
2025-03-17 20:21:53 -0500

    Don't allow card tooltips to go off of the screen

--- scripts/Cards/Classes/card_tooltip.gd
+++ scripts/Cards/Classes/card_tooltip.gd
@@ -13,14 +13,41 @@ func _ready() -> void:
 	self.scale = Vector2(0, 0)  # Start hidden
 	name_label.text = str(cardDisplay.card.card_name)
 	damage_label.text = str(cardDisplay.card.damage, " dmg")
+	adjust_position()
+
 
 func _process(delta: float) -> void:
 	self.scale = self.scale.lerp(target_scale, scale_speed * delta)
 
 func show_tooltip():
+	adjust_position()
 	self.visible = true
 	scale_speed = 20.0
 	target_scale = Vector2(1, 1) / Vector2(cardDisplay.scale[0], cardDisplay.scale[1])
+
+
+func adjust_position():
+	var viewport_size = get_viewport().size
+	var margin = 5
+	var area_global_position = $Card/Area2D.global_position
+	var area_size = $Card/Area2D.get_node("CollisionShape2D").shape.extents * 2
+
+	# Calculate the edges of the tooltip
+	var left_edge = area_global_position.x
+	var right_edge = area_global_position.x + area_size.x
+	var top_edge = area_global_position.y - (area_size.y/2)
+	var bottom_edge = area_global_position.y + (area_size.y/2)
+
+	# Adjust position to ensure the tooltip stays within the viewport
+	if left_edge < margin:
+		self.position.x += margin - left_edge
+	elif right_edge > viewport_size.x - margin:
+		self.position.x -= right_edge - (viewport_size.x - margin)
+
+	if top_edge < margin:
+		self.position.y = margin + area_size.y/4
+	elif bottom_edge > viewport_size.y - margin:
+		self.position.y -= bottom_edge - (viewport_size.y - margin)
 
 func hide_tooltip():
 	target_scale = Vector2(0, 0)
