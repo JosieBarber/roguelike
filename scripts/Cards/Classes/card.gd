d5c514a3c8f1ff75d8722f918da0bdea40106f97
JosieBarber <barbe497@umn.edu>
2025-04-01 13:45:14 -0500

    Fix a bug with regular card setup

--- scripts/Cards/Classes/card.gd
+++ scripts/Cards/Classes/card.gd
@@ -14,12 +14,14 @@ var items: Array
 var effect_methods: Array
 
 func apply_effect(target, source) -> void:
-	var adjusted_damage = calculate_damage(target, source, target.hand.size(),items)
+
+	var adjusted_damage = calculate_damage(target, source, damage,items)
 	target.set_health(target.max_health, target.health - adjusted_damage)
 	print(target.name, " was hit with ", card_name, ", and took ", adjusted_damage, " damage.")
 
 	
 func calculate_damage(target, source, damage: int, items: Array) -> int:
+	print("Damage before modification: ", damage)
 	for item in items:
 		if item.has_method("_modify_damage"):
 			item._modify_damage(damage)
--- scripts/Cards/TestCards/soul_extraction_card.gd
+++ scripts/Cards/TestCards/soul_extraction_card.gd
@@ -17,6 +17,6 @@ func apply_effect(target, source) -> void:
 	var rng = RandomNumberGenerator.new()
 	var card_index = rng.randi_range(0, target.active_deck.size() - 1)
 	
-	source.active_deck.insert_at(card_index, target.active_deck)
+	source.active_deck.insert(card_index, target.active_deck)
 	target.active_deck.remove_at(card_index)
 	
