91e654dd05e22793c70715c7446b70b7751beb6c
JosieBarber <barbe497@umn.edu>
2025-04-10 00:08:42 -0500

    Add Health Labels to Player and enemy

--- scripts/UI/npc_ui.gd
+++ scripts/UI/npc_ui.gd
@@ -1,24 +1,11 @@
 extends Node2D
 
 var enemy: Enemy
-
-func _ready():
-	pass
-	# Try to connect to the enemy's signal if the enemy exists
-	# _try_connect_to_enemy()
-
-# func _try_connect_to_enemy():
-# 	if get_parent().get_parent().has_node("Combat"):
-# 		enemy = get_parent().get_parent().get_node("Combat").get_node("Enemy")
-# 		enemy.connect("enemy_health_changed", Callable(self, "_on_enemy_health_changed"))
-# 		print("connected UI to enemy")
-# 	else:
-# 		# Retry after a short delay if the enemy is not yet available
-# 		await get_tree().create_timer(0.1).timeout
-# 		_try_connect_to_enemy()
+@onready var health_label = $MetalPanel/Health
 
 func _on_enemy_health_changed(new_max_health: int, new_health: int):
 	var hearts_node = get_node("MetalPanel").get_node("Hearts")
+	health_label.text = str(new_health)
 	hearts_node.max_health = new_max_health
 	hearts_node.current_health = new_health
 	hearts_node.update_hearts()
--- scripts/UI/pc_ui.gd
+++ scripts/UI/pc_ui.gd
@@ -5,10 +5,13 @@ var player: Player
 var max_health: int 
 var health: int
 
+@onready var health_label = $MetalPanel/Health
+
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
 	max_health = player.max_health
 	health = player.health
+	health_label.text = str(player.health)
 	player.connect("player_health_changed", Callable(self, "_on_player_health_changed"))
 
 func _on_player_health_changed(new_max_health: int, new_health: int):
@@ -18,6 +21,7 @@ func _on_player_health_changed(new_max_health: int, new_health: int):
 
 func update_hearts():
 	var hearts_node = get_node("MetalPanel").get_node("Hearts")
+	health_label.text = str(player.health)
 	hearts_node.max_health = max_health
 	hearts_node.current_health = health
 	hearts_node.update_hearts()
