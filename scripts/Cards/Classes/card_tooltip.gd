26faec49177c4175ecc0ba3578b7e1fe4b61c484
JosieBarber <barbe497@umn.edu>
2025-04-09 23:36:16 -0500

    Improves card tooltip display and positioning
    Reworks the card tooltip to display information on either the left or right side of the card, depending on screen position.
    
    Ensures tooltip is visible on screen by adjusting its position and switching sides based on viewport boundaries.

--- scripts/Cards/Classes/card_tooltip.gd
+++ scripts/Cards/Classes/card_tooltip.gd
@@ -5,8 +5,9 @@ var target_scale = Vector2(0, 0)
 var scale_speed = 20.0
 
 
-@onready var damage_label = $Card/Damage
-@onready var name_label = $Card/Name
+#@onready var damage_label = $Card/Damage
+@onready var name_labels = [$TooltipRight/Name,$TooltipLeft/Name]
+@onready var effect_labels = [$TooltipRight/Effect,$TooltipLeft/Effect]
 
 @onready var card_sprite = $Card
 
@@ -16,8 +17,13 @@ func _ready() -> void:
 	self.scale = Vector2(0, 0)  # Start hidden
 	self.position = cardDisplay.global_position
 	card_sprite.texture = load(cardDisplay.card.sprite)
-	name_label.text = str(cardDisplay.card.card_name)
-	damage_label.text = str(cardDisplay.card.damage, " dmg")
+	for label in name_labels:
+		label.text = str(cardDisplay.card.card_name)
+	for label in effect_labels:
+		label.text = str(cardDisplay.card.effect)
+	#name_label.text = str(cardDisplay.card.card_name)
+	#effect.text = str(cardDisplay.card.effect)
+	#damage_label.text = str(cardDisplay.card.damage, " dmg")
 	adjust_position()
 
 
@@ -34,7 +40,8 @@ func show_tooltip():
 
 
 func adjust_position():
-	var viewport_size = get_viewport().size
+	var viewport_rect = get_viewport().get_visible_rect()
+	var viewport_size = viewport_rect.size
 	var margin = 5
 	var area_global_position = $Card/Area2D.global_position
 	var area_size = $Card/Area2D.get_node("CollisionShape2D").shape.extents * 2
@@ -55,6 +62,15 @@ func adjust_position():
 		self.position.y = margin + area_size.y/4
 	elif bottom_edge > viewport_size.y - margin:
 		self.position.y -= bottom_edge - (viewport_size.y - margin)
+
+	# Switch tooltip side based on position relative to the middle of the screen
+	var screen_middle = viewport_size.x / 2
+	if self.global_position.x > screen_middle:
+		$TooltipRight.visible = false
+		$TooltipLeft.visible = true
+	else:
+		$TooltipRight.visible = true
+		$TooltipLeft.visible = false
 
 func hide_tooltip():
 	target_scale = Vector2(0, 0)
