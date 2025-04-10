5f9f2b9432cf2abe3ea59fd5a9d9560ae2f736c1
JosieBarber <barbe497@umn.edu>
2025-04-09 22:13:53 -0500

    Improves card hover detection in inventory
    Updates the card hover detection logic in the player inventory.
    
    Now correctly determines if a card is hoverable based on its
    intersection with the inventory mask and the mouse position
    within the inventory bounds.

--- scripts/ClinicScripts/health_shop.gd
+++ scripts/ClinicScripts/health_shop.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var player_inventory_mask = $PlayerInventory/Mask
+@onready var player_inventory_sprite = $PlayerInventory
 @onready var mask_hitbox = $PlayerInventory/Area2D/MaskHitbox
 @onready var shopping_cart_object = $ClinicHeart/ShoppingCart
 @onready var ready_button = $ReadyButton
@@ -75,9 +76,25 @@ func _process(delta: float) -> void:
 	
 	# Calculate the global rectangle of PlayerInventory
 	var inventory_global_rect = Rect2(
+		player_inventory_sprite.global_position - player_inventory_sprite.texture.get_size() / 2,
+		player_inventory_sprite.texture.get_size()
+	)
+
+	var inventory_global_mask_rect = Rect2(
 		player_inventory_mask.global_position - player_inventory_mask.texture.get_size() / 2,
 		player_inventory_mask.texture.get_size()
 	)
+
+	for card_display in player_inventory_mask.get_children():
+		if card_display is Node2D and card_display.has_node("Area2D/CollisionShape2D"):
+			var collision_shape = card_display.get_node("Area2D/CollisionShape2D") as CollisionShape2D
+			if collision_shape and collision_shape.shape is RectangleShape2D:
+				var rect_size = (collision_shape.shape as RectangleShape2D).size
+				var card_global_rect = Rect2(
+					card_display.global_position - rect_size / 2,
+					rect_size
+				)
+				card_display.hoverable = card_global_rect.intersects(inventory_global_mask_rect) and inventory_global_rect.has_point(get_global_mouse_position())
 
 	# Check if the cursor is within the PlayerInventory
 	if inventory_global_rect.has_point(get_global_mouse_position()):
