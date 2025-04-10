c6c03dece5737ccd319899c7af8b66bc3ed58773
JosieBarber <barbe497@umn.edu>
2025-04-10 00:24:06 -0500

    Implements the shopkeeper icon

--- scripts/UI/npc_ui.gd
+++ scripts/UI/npc_ui.gd
@@ -2,9 +2,21 @@ extends Node2D
 
 var enemy: Enemy
 @onready var health_label = $MetalPanel/Health
+@onready var hearts_node = $MetalPanel/Hearts
+@export var npcIcon: Sprite2D
+
+func initialize_for_clinic():
+	health_label.visible = false
+	hearts_node.visible = false
+	npcIcon.frame = 1
+
+func initialize_for_enemy():
+	health_label.visible = true
+	hearts_node.visible = true
+	npcIcon.frame = 0
+
 
 func _on_enemy_health_changed(new_max_health: int, new_health: int):
-	var hearts_node = get_node("MetalPanel").get_node("Hearts")
 	health_label.text = str(new_health)
 	hearts_node.max_health = new_max_health
 	hearts_node.current_health = new_health
